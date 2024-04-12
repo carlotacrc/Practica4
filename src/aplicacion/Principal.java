fb1613298a67e8d2653a3e17f0a9017d58fdcbee
CARLOTA RUIZ CAMPESINO <carlota.ruizcampesino@usp.ceu.es>
2024-04-12 09:55:17 +0200

    Programa acabado

--- src/aplicacion/Principal.java
+++ src/aplicacion/Principal.java
@@ -1,8 +1,21 @@
 package aplicacion;
+/**
+ * Clase principal de la aplicación
+ */
 public class Principal {
+    /**
+     * Método principal de la aplicación
+     * @param args
+     */
     public static void main(String[] args) {
+        /**
+         * Ejemplo de uso de la calculadora de distancias
+         */
         String s1 = "perro";
         String s2 = "terror";
+        /**
+         * Calculamos la distancia entre las dos cadenas
+         */
         System.out.println("La distancia entre " + s1 + " y " + s2 + " es: " + dominio.CalculadoraDistancias.calcularDistancia(s1, s2));
     }
 }
--- src/dominio/CalculadoraDistancias.java
+++ src/dominio/CalculadoraDistancias.java
@@ -1,36 +1,61 @@
 package dominio;
+/**
+ * Clase que implementa la calculadora de distancias
+ */
 public class CalculadoraDistancias{
     /**
      * Calcula la distancia de edició n, tal y como la expone
      * Jurafsky entre las cadenas ` s1` y ` s2`.
      */
     public static int calcularDistancia(String s1, String s2){
-        // creamos la matriz que tendrá las distancias con el tamaño de los strings
+        /**
+         * Creamos una matriz de tamaño (s1.length() + 1) x (s2.length() + 1) para almacenar las distancias
+         */
         int[][] distanceTable = new int[s1.length() + 1][s2.length() + 1];
 
-        //inicializamos la primera fila
+        /**
+         * Inicializamos la primer fila
+         */
         for (int j = 0; j < distanceTable[0].length; j++) {
             distanceTable[0][j] = j;
         }
-        //inicializamos la primer columna
+        /**
+         * Inicializamos la primer columna
+         */
         for (int i = 0; i < distanceTable.length; i++) {
             distanceTable[i][0] = i;
         }
 
-       // calcular las distancias
+       /**
+        * Llenamos la matriz con las distancias
+        */
     for (int i = 1; i < distanceTable.length; i++) {
+        /**
+         * Recorremos la matriz de izquierda a derecha y de arriba hacía abajo
+         */
         for (int j = 1; j < distanceTable[i].length; j++) {
+            /**
+             * Si los caracteres en la posición i-1 de s1 y j-1 de s2 son iguales, no hay operación necesaria
+             */
             if (s1.charAt(i - 1) == s2.charAt(j - 1)) {
-                distanceTable[i][j] = distanceTable[i - 1][j - 1]; // no hay operación necesaria
+                distanceTable[i][j] = distanceTable[i - 1][j - 1]; 
+                /**
+                 * Si los caracteres son diferentes, se puede eliminar, insertar o reemplazar
+                 */
             } else {
                 int delete = distanceTable[i - 1][j] + 1;
                 int insert = distanceTable[i][j - 1] + 1;
                 int replace = distanceTable[i - 1][j - 1] + 2;
-                distanceTable[i][j] = Math.min(delete, Math.min(insert, replace)); // toma el mínimo entre eliminar, insertar o reemplazar
+                /**
+                 * Tomamos el mínimo de las tres operaciones
+                 */
+                distanceTable[i][j] = Math.min(delete, Math.min(insert, replace));
             }
         }
     }
-
+    /**
+     * La matriz queda de la siguiente forma:
+     */
     return distanceTable[s1.length()][s2.length()]; // retorna la distancia entre las dos cadenas
 }
 }
